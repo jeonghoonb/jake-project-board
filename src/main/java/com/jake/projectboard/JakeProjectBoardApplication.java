750d56cde276b72b79f42acf6a7d2b9e783de09c
jake <bangdjh2@gmail.com>
2023-01-26 22:33:52 +0900

    [#2feature/#2-project-setting] * 프로젝트 초기 구성 * git ignore 설정

---
+++ src/main/java/com/jake/projectboard/JakeProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jake.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakeProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakeProjectBoardApplication.class, args);
+    }
+
+}
