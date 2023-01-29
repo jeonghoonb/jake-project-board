602bd0a8a50fef6f8a9d43b736e07bba5613d709
jake <bangdjh2@gmail.com>
2023-01-29 11:00:01 +0900

    [feature/#4-db-setting] * properties -> yaml * db 접근과 편의성을 위한 yaml 세팅 * jpa auditing을 위한 config 세팅

---
+++ src/main/java/com/jake/projectboard/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.jake.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        // TODO(jake): Spring Security 인증을 활용하게 수정 예정
+        return () -> Optional.of("defaultUser");
+    }
+}
