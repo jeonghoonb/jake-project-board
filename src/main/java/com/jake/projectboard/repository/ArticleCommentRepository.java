5323c44e4884a97c80c4e5724b6b80370d1f038e
jake <bangdjh2@gmail.com>
2023-01-29 16:28:56 +0900

    [feature/#1-api] * Querydsl 지원을 이용하여 API 검색 옵션 추가 (Hal Explorer)

--- src/main/java/com/jake/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/jake/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package com.jake.projectboard.repository;
 
 import com.jake.projectboard.domain.ArticleComment;
+import com.jake.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>, // ArticleComment Entity의 모든 field에 대한 검색 기능 추가
+        QuerydslBinderCustomizer<QArticleComment>   // 기본 검색 외 커스텀 기능을 위해
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
