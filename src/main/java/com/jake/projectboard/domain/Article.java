42d9d55f6301a6304d27e706e9251423e4666039
jake <bangdjh2@gmail.com>
2023-01-29 15:02:00 +0900

    [feature/#4-db-setting] * Table 공통 Field 추출 : createdAt, createdBy, modifiedAt, modifiedBy

--- src/main/java/com/jake/projectboard/domain/Article.java
+++ src/main/java/com/jake/projectboard/domain/Article.java
@@ -22,40 +22,28 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false)
+    private String title;
+
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content;
+
+    @Setter
+    private String hashtag;
 
     @ToString.Exclude
     @OrderBy("id")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Setter
-    @Column(nullable = false)
-    private String title;
-    @Setter
-    @Column(nullable = false, length = 10000)
-    private String content;
-    @Setter
-    private String hashtag;
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
 
     private Article(String title, String content, String hashtag) {
         this.title = title;
@@ -78,4 +66,5 @@ public class Article {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
